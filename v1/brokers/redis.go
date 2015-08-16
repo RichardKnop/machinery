46eddcc757fa3fabc82e2e8e9e22e0db6b6dd4a4
Richard Knop <risoknop@gmail.com>
2015-08-16 23:10:45 +0100

    Using BLPOP/RPUSH to simulate queues with Redis instead of SUBSCRIBE/PUBLISH which is more of a broadcast like functionality

--- v1/brokers/redis.go
+++ v1/brokers/redis.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"sync"
+	"time"
 
-	"github.com/RichardKnop/machinery/Godeps/_workspace/src/github.com/garyburd/redigo/redis"
+	//"github.com/RichardKnop/machinery/Godeps/_workspace/src/github.com/garyburd/redigo/redis"
 	"github.com/RichardKnop/machinery/v1/config"
 	"github.com/RichardKnop/machinery/v1/signatures"
 	"github.com/RichardKnop/machinery/v1/utils"
+	"github.com/garyburd/redigo/redis"
 )
 
 // RedisBroker represents a Redis broker
@@ -16,7 +19,9 @@ type RedisBroker struct {
 	config    *config.Config
 	host      string
 	retryFunc func()
+	quitChan  chan int
 	stopChan  chan int
+	wg        sync.WaitGroup
 }
 
 // NewRedisBroker creates new RedisBroker instance
@@ -29,65 +34,19 @@ func NewRedisBroker(cnf *config.Config, host string) Broker {
 
 // StartConsuming enters a loop and waits for incoming messages
 func (redisBroker *RedisBroker) StartConsuming(consumerTag string, taskProcessor TaskProcessor) (bool, error) {
+	redisBroker.stopChan = make(chan int)
+	redisBroker.quitChan = make(chan int)
+	redisBroker.wg.Add(1)
+	errorsChan := make(chan error)
 	if redisBroker.retryFunc == nil {
 		redisBroker.retryFunc = utils.RetryClosure()
 	}
 
-	conn, err := redisBroker.open()
-	if err != nil {
-		redisBroker.retryFunc()
-		return true, fmt.Errorf("Dial: %s", err) // retry true
-	}
+	go redisBroker.consume(errorsChan, taskProcessor)
 
-	redisBroker.retryFunc = utils.RetryClosure()
-
-	defer conn.Close()
-
-	psc := redis.PubSubConn{Conn: conn}
-	if err := psc.Subscribe(redisBroker.config.DefaultQueue); err != nil {
-		return true, err // retry true
-	}
-
-	defer psc.Unsubscribe()
-	defer psc.Close()
-
-	redisBroker.stopChan = make(chan int)
-	deliveries := make(chan signatures.TaskSignature)
-	errors := make(chan error)
-
-	log.Print("[*] Waiting for messages. To exit press CTRL+C")
-
-	// Receving goroutine
-	go func() {
-		for {
-			switch n := psc.Receive().(type) {
-			case redis.Message:
-				log.Printf("Received new message: %s", n.Data)
-
-				signature := signatures.TaskSignature{}
-				if err := json.Unmarshal(n.Data, &signature); err != nil {
-					errors <- err
-					continue
-				}
-
-				deliveries <- signature
-			case redis.Subscription:
-				if n.Count == 0 {
-					return
-				}
-			case error:
-				errors <- n
-				continue
-			}
-		}
-	}()
-
-	// Iterate over delivered tasks and process them
 	for {
 		select {
-		case signature := <-deliveries:
-			taskProcessor.Process(&signature)
-		case err := <-errors:
+		case err := <-errorsChan:
 			return true, err // retry true
 		case <-redisBroker.stopChan:
 			return false, nil // retry false
@@ -95,8 +54,68 @@ func (redisBroker *RedisBroker) StartConsuming(consumerTag string, taskProcessor
 	}
 }
 
+func (redisBroker *RedisBroker) consume(errorsChan chan error, taskProcessor TaskProcessor) {
+	defer redisBroker.wg.Done()
+
+	for {
+		conn, err := redisBroker.open()
+		if err != nil {
+			redisBroker.retryFunc()
+			errorsChan <- err
+			return
+		}
+
+		redisBroker.retryFunc = utils.RetryClosure()
+
+		defer conn.Close()
+
+		log.Print("[*] Waiting for messages. To exit press CTRL+C")
+
+		select {
+		case <-redisBroker.quitChan:
+			return
+		default:
+			// Return value of BLPOP is an array. For example:
+			// redis> RPUSH list1 a b c
+			// (integer) 3
+			// redis> BLPOP list1 list2 0
+			// 1) "list1"
+			// 2) "a"
+			multiBulk, err := redis.MultiBulk(conn.Do("BLPOP", redisBroker.config.DefaultQueue, "0"))
+			if err != nil {
+				errorsChan <- err
+				return
+			}
+
+			item, err := redis.Bytes(multiBulk[1], nil)
+			if err != nil {
+				log.Print("BBBBBB")
+				errorsChan <- err
+				return
+			}
+
+			log.Printf("Received new message: %s", item)
+
+			signature := signatures.TaskSignature{}
+			if err := json.Unmarshal(item, &signature); err != nil {
+				errorsChan <- err
+				return
+			}
+
+			if err := taskProcessor.Process(&signature); err != nil {
+				errorsChan <- err
+				return
+			}
+		}
+	}
+}
+
 // StopConsuming quits the loop
 func (redisBroker *RedisBroker) StopConsuming() {
+	// Notifying the quit channel stops receiving goroutine
+	redisBroker.quitChan <- 1
+	// Wait for the receiving goroutine to have stopped
+	redisBroker.wg.Wait()
 	// Notifying the quit channel stops consuming of messages
 	redisBroker.stopChan <- 1
 }
@@ -114,7 +133,7 @@ func (redisBroker *RedisBroker) Publish(signature *signatures.TaskSignature) err
 		return fmt.Errorf("JSON Encode Message: %v", err)
 	}
 
-	_, err = conn.Do("PUBLISH", redisBroker.config.DefaultQueue, message)
+	_, err = conn.Do("RPUSH", redisBroker.config.DefaultQueue, message)
 	return err
 }
 
@@ -124,4 +143,23 @@ func (redisBroker *RedisBroker) open() (redis.Conn, error) {
 	// subscribing to a channel, PUBLISH is not allowed on that connection
 	// e.g. ERR only (P)SUBSCRIBE / (P)UNSUBSCRIBE / QUIT allowed in this context
 	return redis.Dial("tcp", redisBroker.host)
+}
+
+// Returns a new pool of Redis connections
+func (redisBroker *RedisBroker) newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", redisBroker.host)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
 }
