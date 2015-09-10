132c753b8484f2f75f468232c5af4ca0807e6ecc
ml <ml@Macintosh.local>
2015-09-10 14:13:41 +0800

    add eager mode
    - an in-memory broker / backend for test/dev purpose
    
    in Config:
    - Broker: “eager”
    - ResultBackend: “eager”

---
+++ v1/brokers/eager.go
@@ -0,0 +1,61 @@
+package brokers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/RichardKnop/machinery/v1/signatures"
+)
+
+type EagerBroker struct {
+	worker TaskProcessor
+}
+
+func NewEagerBroker() Broker {
+	return &EagerBroker{}
+}
+
+type EagerMode interface {
+	AssignWorker(p TaskProcessor)
+}
+
+//
+// Broker interface
+//
+func (e *EagerBroker) StartConsuming(consumerTag string, p TaskProcessor) (bool, error) {
+	return true, nil
+}
+
+func (e *EagerBroker) StopConsuming() {
+	// do nothing
+}
+
+func (e *EagerBroker) Publish(task *signatures.TaskSignature) error {
+	if e.worker == nil {
+		return errors.New("worker is not assigned in eager-mode")
+	}
+
+	// faking the behavior to marshal input into json
+	// and unmarshal it back
+	message, err := json.Marshal(task)
+	if err != nil {
+		return errors.New(fmt.Sprintf("json marshaling failed: %v", err))
+	}
+
+	sig_ := signatures.TaskSignature{}
+	err = json.Unmarshal(message, &sig_)
+	if err != nil {
+		return errors.New(fmt.Sprintf("json unmarshaling failed: %v", err))
+	}
+
+	// blocking call to the task directly
+	return e.worker.Process(&sig_)
+}
+
+//
+// Eager interface
+//
+func (e *EagerBroker) AssignWorker(p TaskProcessor) {
+	e.worker = p
+}
