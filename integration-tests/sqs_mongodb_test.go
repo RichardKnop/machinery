83d61ea6c7f544c073492f356b672af45753fba3
Lee Fernandes <itsleeowen@gmail.com>
2018-02-17 15:32:29 -0500

    move sqs env vars to yml

--- integration-tests/sqs_mongodb_test.go
+++ integration-tests/sqs_mongodb_test.go
@@ -12,12 +12,13 @@ func TestSQSMongodb(t *testing.T) {
 	sqsURL := os.Getenv("SQS_URL")
 	mongodbURL := os.Getenv("MONGODB_URL")
 	if sqsURL == "" || mongodbURL == "" {
+		t.Error("shhhiiiit", sqsURL)
 		return
 	}
 
 	// AMQP broker, MongoDB result backend
 	server := testSetup(&config.Config{
-		Broker:        "https://sqs.us-east-2.amazonaws.com/155825796015",
+		Broker:        sqsURL,
 		DefaultQueue:  "test_queue",
 		ResultBackend: fmt.Sprintf("mongodb://%v", mongodbURL),
 	})
