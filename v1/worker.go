72755f032cec79388ebc4b5a2b14934f7e1d2d0a
Jackie Li <jackie.li@fuseim.com>
2019-04-03 22:28:08 +0800

    inject signiture into context

--- v1/worker.go
+++ v1/worker.go
@@ -135,7 +135,7 @@ func (worker *Worker) Process(signature *tasks.Signature) error {
 	}
 
 	// Prepare task for processing
-	task, err := tasks.New(taskFunc, signature.Args)
+	task, err := tasks.NewWithSignature(taskFunc, signature)
 	// if this failed, it means the task is malformed, probably has invalid
 	// signature, go directly to task failed without checking whether to retry
 	if err != nil {
