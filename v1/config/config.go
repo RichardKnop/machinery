9ab3f2fc060ac11b6c47cd201d681158dd1c45e1
Nicolas Walter <nicolas.walter@brubank.com>
2019-06-11 13:11:59 -0300

    Add param to AMQPConfig in order to be able to autodelete queues.

--- v1/config/config.go
+++ v1/config/config.go
@@ -77,6 +77,8 @@ type AMQPConfig struct {
 	QueueBindingArgs QueueBindingArgs `yaml:"queue_binding_args" envconfig:"AMQP_QUEUE_BINDING_ARGS"`
 	BindingKey       string           `yaml:"binding_key" envconfig:"AMQP_BINDING_KEY"`
 	PrefetchCount    int              `yaml:"prefetch_count" envconfig:"AMQP_PREFETCH_COUNT"`
+	AutoDelete		 bool			  `yaml:"auto_delete" envconfig:"AMQP_AUTO_DELETE"`
+
 }
 
 // DynamoDBConfig wraps DynamoDB related configuration
