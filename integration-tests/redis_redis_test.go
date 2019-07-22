aed5cdd9b17ed8f130a1488f117cdfb54ae3f240
Pavle Lee <523260513@qq.com>
2019-07-22 10:47:00 +0800

    add NormalTasksPollPeriod test case

--- integration-tests/redis_redis_test.go
+++ integration-tests/redis_redis_test.go
@@ -2,8 +2,12 @@ package integration_test
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/RichardKnop/machinery/v1/config"
 )
@@ -24,4 +28,44 @@ func TestRedisRedis(t *testing.T) {
 	go worker.Launch()
 	testAll(server, t)
 	worker.Quit()
+}
+
+func TestRedisRedis_NormalTasksPollPeriod(t *testing.T) {
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		t.Skip("REDIS_URL is not defined")
+	}
+
+	normalTasksPollPeriod := 5
+
+	// Redis broker, Redis result backend
+	var server = testSetup(&config.Config{
+		Broker:        fmt.Sprintf("redis://%v", redisURL),
+		DefaultQueue:  "test_queue",
+		ResultBackend: fmt.Sprintf("redis://%v", redisURL),
+		Redis: &config.RedisConfig{
+			MaxIdle:                3,
+			IdleTimeout:            240,
+			ReadTimeout:            15,
+			WriteTimeout:           15,
+			ConnectTimeout:         15,
+			DelayedTasksPollPeriod: 20,
+			NormalTasksPollPeriod:  normalTasksPollPeriod, // Affected by ReadTimeout
+		},
+	})
+	worker := server.NewWorker("test_worker", 0)
+	errorChan := make(chan error)
+	worker.LaunchAsync(errorChan)
+
+	testAll(server, t)
+
+	before := time.Now()
+	worker.Quit()
+	after := time.Now()
+
+	d := after.Sub(before)
+	b := math.Ceil(d.Seconds())
+
+	// Including other time, so round up
+	assert.Equal(t, b, float64(normalTasksPollPeriod+1))
 }
