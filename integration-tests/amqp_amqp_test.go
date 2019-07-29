dc01f0e93c30b5529277648b1de7e35dda89283f
Patrik Cyvoct <patrik@ptrk.io>
2019-07-29 09:47:26 +0200

    Add test for multiple amqp brokers
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- integration-tests/amqp_amqp_test.go
+++ integration-tests/amqp_amqp_test.go
@@ -13,11 +13,25 @@ func TestAmqpAmqp(t *testing.T) {
 		t.Skip("AMQP_URL is not defined")
 	}
 
+	finalAmqpURL := amqpURL
+	var finalSeparator string
+
+	amqpURLs := os.Getenv("AMQP_URLS")
+	if amqpURLs != "" {
+		separator := os.Getenv("AMQP_URLS_SEPARATOR")
+		if separator == "" {
+			return
+		}
+		finalSeparator = separator
+		finalAmqpURL = amqpURLs
+	}
+
 	// AMQP broker, AMQP result backend
 	server := testSetup(&config.Config{
-		Broker:        amqpURL,
-		DefaultQueue:  "test_queue",
-		ResultBackend: amqpURL,
+		Broker:                  finalAmqpURL,
+		MultipleBrokerSeparator: finalSeparator,
+		DefaultQueue:            "test_queue",
+		ResultBackend:           amqpURL,
 		AMQP: &config.AMQPConfig{
 			Exchange:      "test_exchange",
 			ExchangeType:  "direct",
