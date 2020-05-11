5a6bfdd2848e89c5cb3a0619e257c4c600297005
Richard Knop <RichardKnop@users.noreply.github.com>
2020-05-11 14:36:05 +0100

    feat: use v2 in example, small refactor of v2 experiment (#544)

--- v2/worker_test.go
+++ v2/worker_test.go
@@ -1,10 +1,20 @@
 package machinery_test
 
 import (
-	"github.com/RichardKnop/machinery/v2"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/RichardKnop/machinery/v2"
 )
+
+func TestRedactURL(t *testing.T) {
+	t.Parallel()
+
+	broker := "amqp://guest:guest@localhost:5672"
+	redactedURL := machinery.RedactURL(broker)
+	assert.Equal(t, "amqp://localhost:5672", redactedURL)
+}
 
 func TestPreConsumeHandler(t *testing.T) {
 	t.Parallel()
