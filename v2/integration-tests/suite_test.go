53e2e28161717cf6185645f080214577dcf0edfa
xiaozuo7 <49382984+xiaozuo7@users.noreply.github.com>
2021-08-06 23:56:29 +0800

    v2_unused_function_delete (#689)
    * Docs fix
    
    I observed that the code in the `V2 Experiment` snippet does not apply to the v2 version, so I made some updates, and I noticed that there is no error in the return parameter of function `machinery.NewServer()`, do you consider adding exception handling?
    
    * specification_revision
    
    * issue#687_fix
    
    * issue#687_v1_fix
    
    * recover
    
    * delete_unused_function
    
    * v2_server_delete_unused_function

--- v2/integration-tests/suite_test.go
+++ v2/integration-tests/suite_test.go
@@ -3,7 +3,6 @@ package integration_test
 import (
 	"context"
 	"errors"
-	"log"
 	"reflect"
 	"sort"
 	"testing"
@@ -11,7 +10,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/RichardKnop/machinery/v2"
 	"github.com/RichardKnop/machinery/v2/backends/result"
 	"github.com/RichardKnop/machinery/v2/config"
 	"github.com/RichardKnop/machinery/v2/tasks"
@@ -388,18 +386,6 @@ func registerTestTasks(server Server) {
 	}
 
 	server.RegisterTasks(tasks)
-}
-
-func testSetup(cnf *config.Config) Server {
-
-	server, err := machinery.NewServer(cnf)
-	if err != nil {
-		log.Fatal(err, "Could not initialize server")
-	}
-
-	registerTestTasks(server)
-
-	return server
 }
 
 func newAddTask(a, b int) *tasks.Signature {
