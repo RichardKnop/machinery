c57ceed9c02d00238a95fb2f33ddd6880dd706b6
ggodik <ggodik@factset.com>
2022-04-04 17:41:16 -0400

    fix(windows): removing windows specific code

--- v1/utils/utils.go
+++ v1/utils/utils.go
@@ -3,20 +3,14 @@ package utils
 import (
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 )
 
 const (
 	LockKeyPrefix = "machinery_lock_"
-	windows       = "windows"
 )
 
 func GetLockName(name, spec string) string {
 	cmd := filepath.Base(os.Args[0])
-	if runtime.GOOS == windows {
-		cmd = strings.ReplaceAll(cmd, ".exe", "")
-	}
-
 	return strings.Join([]string{LockKeyPrefix, cmd, name, spec}, "")
 }
