f2b68e7e0317c9b6d84cb1235f3755687b35e751
ggodik <ggodik@factset.com>
2022-04-04 16:53:07 -0400

    fix(windows): fixing tests on windows

--- v1/utils/utils.go
+++ v1/utils/utils.go
@@ -3,12 +3,20 @@ package utils
 import (
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 const (
 	LockKeyPrefix = "machinery_lock_"
+	windows       = "windows"
 )
 
 func GetLockName(name, spec string) string {
-	return LockKeyPrefix + filepath.Base(os.Args[0]) + name + spec
+	cmd := filepath.Base(os.Args[0])
+	if runtime.GOOS == windows {
+		cmd = strings.ReplaceAll(cmd, ".exe", "")
+	}
+
+	return strings.Join([]string{LockKeyPrefix, cmd, name, spec}, "")
 }
