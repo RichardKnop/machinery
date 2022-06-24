9d53974460c52bbef0f31302d7411244ca6178ea
jarch09 <jarch09@gmail.com>
2022-06-24 12:34:32 -0400

    switching textmap headers to http headers

--- v2/tasks/signature.go
+++ v2/tasks/signature.go
@@ -2,8 +2,10 @@ package tasks
 
 import (
 	"fmt"
-	"github.com/RichardKnop/machinery/v2/utils"
+	"net/http"
 	"time"
+
+	"github.com/RichardKnop/machinery/v2/utils"
 
 	"github.com/google/uuid"
 )
@@ -15,33 +17,6 @@ type Arg struct {
 	Value interface{} `bson:"value"`
 }
 
-// Headers represents the headers which should be used to direct the task
-type Headers map[string]interface{}
-
-// Set on Headers implements opentracing.TextMapWriter for trace propagation
-func (h Headers) Set(key, val string) {
-	h[key] = val
-}
-
-// ForeachKey on Headers implements opentracing.TextMapReader for trace propagation.
-// It is essentially the same as the opentracing.TextMapReader implementation except
-// for the added casting from interface{} to string.
-func (h Headers) ForeachKey(handler func(key, val string) error) error {
-	for k, v := range h {
-		// Skip any non string values
-		stringValue, ok := v.(string)
-		if !ok {
-			continue
-		}
-
-		if err := handler(k, stringValue); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // Signature represents a single task invocation
 type Signature struct {
 	UUID           string
@@ -51,7 +26,7 @@ type Signature struct {
 	GroupUUID      string
 	GroupTaskCount int
 	Args           []Arg
-	Headers        Headers
+	Headers        http.Header
 	Priority       uint8
 	Immutable      bool
 	RetryCount     int
