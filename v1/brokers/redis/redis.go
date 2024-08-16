e3ac2094360d344ae0cc6b3869f563086a420c54
rui.xiao <rui.xiao@shopee.com>
2024-08-16 15:13:56 +0800

    fix redis consumer bug

--- v1/brokers/redis/redis.go
+++ v1/brokers/redis/redis.go
@@ -91,6 +91,7 @@ func (b *Broker) StartConsuming(consumerTag string, concurrency int, taskProcess
 	// Channel to which we will push tasks ready for processing by worker
 	deliveries := make(chan []byte, concurrency)
 	pool := make(chan struct{}, concurrency)
+	stopConsumer := make(chan struct{})
 
 	// initialize worker pool with maxWorkers workers
 	for i := 0; i < concurrency; i++ {
@@ -101,13 +102,15 @@ func (b *Broker) StartConsuming(consumerTag string, concurrency int, taskProcess
 	// If the message is valid and can be unmarshaled into a proper structure
 	// we send it to the deliveries channel
 	go func() {
-
 		log.INFO.Print("[*] Waiting for messages. To exit press CTRL+C")
 
 		for {
 			select {
 			// A way to stop this goroutine from b.StopConsuming
 			case <-b.GetStopChan():
+				close(deliveries)
+				return
+			case <-stopConsumer:
 				close(deliveries)
 				return
 			case <-pool:
@@ -142,6 +145,8 @@ func (b *Broker) StartConsuming(consumerTag string, concurrency int, taskProcess
 			// A way to stop this goroutine from b.StopConsuming
 			case <-b.GetStopChan():
 				return
+			case <-stopConsumer:
+				return
 			default:
 				task, err := b.nextDelayedTask(b.redisDelayedTasksKey)
 				if err != nil {
@@ -162,7 +167,7 @@ func (b *Broker) StartConsuming(consumerTag string, concurrency int, taskProcess
 		}
 	}()
 
-	if err := b.consume(deliveries, concurrency, taskProcessor); err != nil {
+	if err := b.consume(deliveries, concurrency, taskProcessor, stopConsumer); err != nil {
 		return b.GetRetry(), err
 	}
 
@@ -275,7 +280,7 @@ func (b *Broker) GetDelayedTasks() ([]*tasks.Signature, error) {
 
 // consume takes delivered messages from the channel and manages a worker pool
 // to process tasks concurrently
-func (b *Broker) consume(deliveries <-chan []byte, concurrency int, taskProcessor iface.TaskProcessor) error {
+func (b *Broker) consume(deliveries <-chan []byte, concurrency int, taskProcessor iface.TaskProcessor, stopConsumer chan struct{}) error {
 	errorsChan := make(chan error, concurrency*2)
 	pool := make(chan struct{}, concurrency)
 
@@ -289,6 +294,10 @@ func (b *Broker) consume(deliveries <-chan []byte, concurrency int, taskProcesso
 	for {
 		select {
 		case err := <-errorsChan:
+			close(stopConsumer)
+			for v := range deliveries {
+				b.requeueMessage(v, taskProcessor)
+			}
 			return err
 		case d, open := <-deliveries:
 			if !open {
