2b47513f79297031edd4fd1e4849d6631f76099f
jaume.bret <jaume.bret@tangelogames.com>
2025-04-23 13:19:12 +0200

    Reorder imports in dynamodb.go for improved organization

--- v2/backends/dynamodb/dynamodb.go
+++ v2/backends/dynamodb/dynamodb.go
@@ -6,14 +6,13 @@ import (
 	"math"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws/session"
-
 	"github.com/RichardKnop/machinery/v2/backends/iface"
 	"github.com/RichardKnop/machinery/v2/common"
 	"github.com/RichardKnop/machinery/v2/config"
 	"github.com/RichardKnop/machinery/v2/log"
 	"github.com/RichardKnop/machinery/v2/tasks"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
